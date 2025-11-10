029be3f6e50577fe6dae84a6a0658c5caee5f4eb
jiayu-pyrexia <121092912+Pyrexiaa@users.noreply.github.com>
2025-11-10 21:14:07 +0800

    Integrated new training algorithm with radar data
    Fixed numerical instability issues while loading weather stations

--- dataset/weather_graph_dataset.py
+++ dataset/weather_graph_dataset.py
@@ -79,3 +79,47 @@ class WeatherGraphDatasetWithRadar(Dataset):
       'gen_y': self.data['general_station'].y[idx],
       'rain_y': self.data['rainfall_station'].y[idx],
     }
+  
+class WeatherGraphDatasetWithRadarNew(Dataset):
+  def __init__(self, data, mode = Literal['train', 'test', 'val'], device='cpu'):
+    assert mode in ['train', 'test', 'val'], f"Invalid mode: must be either 'train' or 'test'."
+
+    self.data = data.to(device)
+    self.mode = mode
+    self.device = device
+    self.num_timesteps = data['general_station'].x.shape[0]
+    self.edge_index_dict = {
+        key:val for key, val in self.data.edge_index_dict.items()
+    }
+
+    self.edge_attribute_dict = {
+      key:val.to(device) for key, val in data.edge_attr_dict.items()
+    }
+
+    if mode == 'train':
+      self.metastation_mask = data['general_station'].train_mask
+      self.rainfallstation_mask = data['rainfall_station'].train_mask
+
+    elif mode == 'val':
+      self.metastation_mask = data['general_station'].val_mask
+      self.rainfallstation_mask = data['rainfall_station'].val_mask
+
+    else: #test
+      self.metastation_mask = data['general_station'].test_mask
+      self.rainfallstation_mask = data['rainfall_station'].test_mask
+
+  def __len__(self):
+    return self.num_timesteps
+  
+  def __getitem__(self, idx):
+    return{
+      'gen_x': self.data['general_station'].x[idx],
+      'rain_x': self.data['rainfall_station'].x[idx],
+      "radar_x": self.data['radar_grid'].x[idx],  # Include radar
+      'gen_y': self.data['general_station'].y[idx],
+      'rain_y': self.data['rainfall_station'].y[idx],
+      'metastation_mask': self.metastation_mask,
+      'rainfallstation_mask': self.rainfallstation_mask,
+      'edge_index_dict': self.edge_index_dict,
+      'edge_attr_dict': self.edge_attribute_dict
+    }
