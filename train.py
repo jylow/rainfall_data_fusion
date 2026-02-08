f37cd81febc817f1cb05212ebde60e851cbab200
jylow <lowjy28@gmail.com>
2026-02-08 12:01:01 +0800

    fix device problem

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ def main(config):
             print("✅ model weights saved to weather_gnn_best.pth")
 
     for i in range(fold_count):
-        train_fold(model_arr[i], train_loader=train_loader_arr[i], val_loader=val_loader_arr[i], fold = i)
+        train_fold(model_arr[i], train_loader=train_loader_arr[i], val_loader=val_loader_arr[i], fold = i, device=device)
         RMSE = test_model(model_arr[i], raingauge_station_mappings_df, test_loader_arr[i], device, fold=i, experiment_name=experiment_name)
 
 if __name__ == '__main__':
